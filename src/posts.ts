c0ccbcf144a38defafd516bdd99a2defdee31978
Nick Morley <nick.morley111@gmail.com>
2022-12-08 11:38:58 +0000

    Update base

--- src/posts.ts
+++ src/posts.ts
@@ -1,9 +1,5 @@
-// import testPost from "../posts/test.md?raw";
-// import testPost2 from "../posts/test-2.md?raw";
 import firstPost from "../posts/first-post.md?raw";
 
 export const postsList = [
-  // testPost,
-  // testPost2,
   firstPost,
 ];
